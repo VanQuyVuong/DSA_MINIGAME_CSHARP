a0ab3ea489c4850a3666d9a8bb23c4a15d631e3d
VANQUYVUONG <vanbin1605@gmail.com>
2026-02-25 23:31:23 +0700

    Learn Bai1 BubbleSort (Sắp xếp nổi bọt)

---
+++ Learn_DSA/BUBBLESORT.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Learn_DSA
+{
+    internal class BUBBLESORT
+    {
+        public static void BubbleSort(int[] a)
+        {
+            int n = a.Length;
+
+            for (int i = 0; i < n - 1; i++)
+            {
+                for (int j = 0; j < n - i - 1; j++)
+                {
+                    if (a[j] > a[j + 1])
+                    {
+                        int temp = a[j];
+                        a[j] = a[j + 1];
+                        a[j + 1] = temp;
+                    }
+                }
+            }
+
+            Console.WriteLine(string.Join(" ", a));
+        }
+    }
+}
--- Learn_DSA/Program.cs
+++ Learn_DSA/Program.cs
@@ -1,2 +1,41 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Text;
+
+
+namespace Learn_DSA
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = Encoding.UTF8;
+            Console.InputEncoding = Encoding.UTF8;
+
+            Console.WriteLine("Nhập số lượng phần tử:");
+            int sl = int.Parse(Console.ReadLine());
+
+            int[] a = new int[sl];
+
+            Console.WriteLine("Nhập các số:");
+            for (int i = 0; i < sl; i++)
+            {
+                Console.WriteLine($"Phần tử {i + 1}:");
+                a[i] = int.Parse(Console.ReadLine());
+            }
+
+            Console.Clear();
+
+            Console.WriteLine("Chọn thuật toán:");
+            Console.WriteLine("1. Sắp xếp nổi bọt");
+
+            int choice = int.Parse(Console.ReadLine());
+
+            switch (choice)
+            {
+                case 1:
+                    BUBBLESORT.BubbleSort(a);
+                    break;
+            }
+        }
+    }
+}
